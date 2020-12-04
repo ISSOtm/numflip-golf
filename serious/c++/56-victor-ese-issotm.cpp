e900d1f200f7f1c84570c20ad93ba26872537efb
ISSOtm <eldredhabert0@gmail.com>
2020-12-04 22:25:47 +0100

    Add alternative to Victor's C++56, by Victor
    This is getting a bit ridiculous, eh?

--- serious/c++/56-victor-ese-issotm.cpp
+++ serious/c++/56-victor-ese-issotm.cpp
@@ -1,3 +1,5 @@
 int f(int x){int y=0;for(;x;x/=10)y=y*10+x%10;return y;}
 // Alternative by Ese & ISSOtm
 int f(int x){int y=0;while(y=y*10+x%10,x/=10);return y;}
+// Alternative alternative by Victor
+int f(int x){int y=0;for(;y=y*10+x%10,x/=10;);return y;}
